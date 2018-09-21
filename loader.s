7ed03ce499c1b157d85459cda6e1e4dc683c950c
bolhoso <lucasrosada@gmail.com>
2018-09-21 01:31:35 -0400

    Adds Makefile to kernel build
    Removed housemade compilation scripts and put a Makefile in place

--- loader.s
+++ loader.s
@@ -1,8 +1,14 @@
 global loader
 
-MAGIC_NUMBER    equ 0x1BADB002 ; the multiboot magic number spec:w
-FLAGS           equ 0x0
-CHECKSUM        equ -MAGIC_NUMBER
+MAGIC_NUMBER      equ 0x1BADB002 ; the multiboot magic number spec:w
+FLAGS             equ 0x0
+CHECKSUM          equ -MAGIC_NUMBER
+KERNEL_STACK_SIZE equ 4096
+
+section .bss
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE
 
 section .text
 align 4
@@ -12,6 +18,8 @@ align 4
 
 loader:
     mov eax, 0xCAFEBABE
+    mov esp, kernel_stack + KERNEL_STACK_SIZE ; point esp to the start of the
+                                              ; stack (end of memory area)
+
 .loop:
     jmp .loop           ; I like the CPU hot babe
-
